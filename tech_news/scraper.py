4efd4390a32a4739e04e4e2f3162ddc35b2c4200
EzequielVerissimoCarvalhoZica <ezeverissimo@gmail.com>
2022-07-19 15:49:45 -0300

    feat: function scrape next page - req 03

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -31,7 +31,10 @@ def scrape_novidades(html_content):
 
 # Requisito 3
 def scrape_next_page_link(html_content):
-    """Seu código deve vir aqui"""
+    selector = Selector(text=html_content)
+    next_page = selector.css("a.next::attr(href)").get()
+
+    return next_page
 
 
 # Requisito 4
