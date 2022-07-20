df0e23d6aa3442d42d4fbf80d64c53b376849753
EzequielVerissimoCarvalhoZica <ezeverissimo@gmail.com>
2022-07-20 08:50:30 -0300

    feat: function search by title - req 06

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,6 +1,13 @@
+import re
+from tech_news.database import search_news
+
+
 # Requisito 6
-def search_by_title(title):
-    """Seu código deve vir aqui"""
+def search_by_title(title: str):
+    noticies = search_news({"title": re.compile(title, re.IGNORECASE)})
+    tuple_news = [(notice["title"], notice["url"]) for notice in noticies]
+
+    return tuple_news
 
 
 # Requisito 7
